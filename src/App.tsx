c4594620164b8411b762f3ae0c3794d78e6d0765
donghhan <apensia914@gmail.com>
2022-11-22 08:57:40 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>This is app</h1>
+      <h2>hello</h2>
+      <h3>That works!</h3>
+    </>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProductionMode = process.env.NODE_ENV === "production";
+
+module.exports = {
+  mode: isProductionMode ? "production" : "development",
+  entry: "./src",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+      progress: true,
+    },
+    compress: true,
+    hot: true,
+    port: "auto",
+  },
+  devtool: isProductionMode ? false : "eval",
+  module: {
+    rules: [
+      {
+        test: /\.(ts)x?$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      minify: isProductionMode
+        ? {
+            collapseWhitespace: true,
+            removeComments: true,
+            keepClosingSlash: false,
+          }
+        : false,
+    }),
+  ],
+};
