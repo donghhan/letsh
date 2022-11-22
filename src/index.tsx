eae455c68536e8b55010d5f493ab1e32ba73d9f5
donghhan <apensia914@gmail.com>
2022-11-22 16:54:08 +0900

    Pages structuring

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,38 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import HomeLayout from "./components/pages/home/HomeLayout";
+import AuthLayout from "./components/pages/users/auth/AuthLayout";
+import NotFoundPage from "./components/pages/etc/NotFoundPage";
+import WelcomePage from "./components/pages/home/WelcomePage";
+import LoginPage from "./components/pages/users/auth/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "",
+        element: <WelcomePage />,
+      },
+    ],
+  },
+]);
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
